5b85e47270db44499661226d4f6c97dbcf2c3a8b
Zeha Irawan <jangkaringbumi@gmail.com>
2020-08-25 19:33:45 +0700

    add proxy and create Contact component

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,11 +1,64 @@
-import React from 'react';
-import Navbar from '../Navbar';
+import axios from 'axios';
+import React, { useState } from 'react';
+import Navbar from './Navbar'
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    subject: '',
+    content: '',
+  });
+
+  const { subject, content } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      axios({
+        method: 'post',
+        url: '/send',
+        data: {
+          subject,
+          content,
+        },
+      });
+      alert('Email sent successfully!');
+    } catch (error) {
+      alert(`Email failed,${error} `);
+    }
+  };
+
   return (
     <div>
       <Navbar></Navbar>
-      <input type="text" placeholder="hello" name="" id="" />
+      <form onSubmit={onSubmit}>
+        <div>
+          <p>Topic</p>
+          <input
+            type="text"
+            placeholder="Subject"
+            name="subject"
+            value={subject}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div>
+          <p>Content</p>
+          <input
+            type="text"
+            placeholder="Content"
+            name="content"
+            value={content}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
 };
