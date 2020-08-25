cb45a6d528d321f5d8f735be695c22887dfd782d
Zeha Irawan <jangkaringbumi@gmail.com>
2020-08-25 10:35:34 +0700

    remove test endpoint

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/', (req,res) => res.send('API Running'))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
