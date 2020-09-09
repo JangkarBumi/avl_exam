38017ab5479d94a020a244c4933e99004df88470
Zeha Irawan <jangkaringbumi@gmail.com>
2020-09-09 19:50:28 +0700

    chaining filter

--- client/src/components/QuestionList.js
+++ client/src/components/QuestionList.js
@@ -2,44 +2,74 @@ import { Button } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { db } from '../firebase/firebaseConfig';
 
-const QuestionList = ({ match }) => {
+const QuestionList = () => {
   const [questions, setQuestions] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [calculatorFilter, setCalculatorFilter] = useState('');
+  const [answerTypeFilter, setAnswerTypeFilter] = useState('');
+  const [chartFilter, setChartFilter] = useState('');
+  const [lengthFilter, setLengthFilter] = useState('');
 
   useEffect(() => {
-    function getData() {
-      db.collection('problems')
-        .get()
-        .then((querySnapshot) => {
-          const data = querySnapshot.docs.map((doc) => doc.data());
-          setQuestions(data);
-          setLoading(false);
-        });
+    async function getData() {
+      const problemsRef = db.collection('problems');
+
+      const snapshot = await problemsRef.get();
+
+      if (snapshot.empty) {
+        console.log('No matching documents.');
+        return;
+      }
+
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+      setQuestions(data);
+      setLoading(false);
     }
     getData();
   }, []);
 
   // Query
 
-  const filterQuestion = async (order, params) => {
+  const filterQuestion = async () => {
     setLoading(true);
+    let query = db.collection('problems');
 
-    const problemsRef = db.collection('problems');
+    if (calculatorFilter.length > 0) {
+      query = query.where(calculatorFilter[0], '==', calculatorFilter[1]);
+    }
 
-    const snapshot = await problemsRef.where(`${order}`, '==', `${params}`).get();
+    if (answerTypeFilter.length > 0) {
+      query = query.where(answerTypeFilter[0], '==', answerTypeFilter[1]);
+    }
+
+    if (chartFilter.length > 0) {
+      query = query.where(chartFilter[0], '==', chartFilter[1]);
+    }
+
+    if (lengthFilter.length > 0) {
+      query = query.where(lengthFilter[0], '==', lengthFilter[1]);
+    }
+
+    // console.log(query);
+
+    const snapshot = await query.get();
 
     if (snapshot.empty) {
       console.log('No matching documents.');
       return;
     }
 
-    const data = []
-    snapshot.forEach(doc => {
-       data.push(doc.data())
+    const data = [];
+    snapshot.forEach((doc) => {
+      data.push(doc.data());
     });
 
-
     setQuestions(data);
+
+    console.log(data.length);
     setLoading(false);
   };
 
@@ -48,51 +78,106 @@ const QuestionList = ({ match }) => {
   return (
     <div>
       <div>
-  <h1>There is {questions.length} Question</h1>
+        <h1>There is {questions.length} Question</h1>
         <h1>More Filters</h1>
 
         <div>
           <h2>Calculator</h2>
-          <button>All</button>
-          <button>Calculator</button>
-          <button>No Calculator</button>
+          <button
+            onClick={() => {
+              setCalculatorFilter('');
+              filterQuestion();
+            }}
+          >
+            All
+          </button>
+          <button
+            onClick={() => {
+              setCalculatorFilter(['calculator', 'Calculator']);
+              filterQuestion();
+            }}
+          >
+            Calculator
+          </button>
+          <button
+            onClick={() => {
+              setCalculatorFilter(['calculator', 'No Calculator']);
+              filterQuestion();
+            }}
+          >
+            No Calculator
+          </button>
         </div>
 
         <div>
           <h2>Answer Type</h2>
-          <button>All</button>
-          <button>Multiple Choice</button>
-          <button>Grid In</button>
+          <button
+            onClick={() => {
+              setAnswerTypeFilter('');
+              filterQuestion();
+            }}
+          >
+            All
+          </button>
+          <button
+            onClick={() => {
+              setAnswerTypeFilter(['answer_type', 'Multiple Choice']);
+              filterQuestion();
+            }}
+          >
+            Multiple Choice
+          </button>
+          <button
+            onClick={() => {
+              setAnswerTypeFilter(['answer_type', 'Grid-In']);
+              filterQuestion();
+            }}
+          >
+            Grid In
+          </button>
         </div>
 
         <div>
           <h2>Chart Type</h2>
           <button>All</button>
-          <button>Contains chart</button>
-          <button>No chart</button>
+          <button
+            onClick={() => {
+              setChartFilter(['chart', 'Chart']);
+              filterQuestion();
+            }}
+          >
+            Contains chart
+          </button>
+          <button
+            onClick={() => {
+              setChartFilter(['chart', 'No Chart']);
+              filterQuestion();
+            }}
+          >
+            No chart
+          </button>
         </div>
 
         <div>
           <h2>Problem Length</h2>
           <button>All</button>
-          <button>Short</button>
-          <button>Long</button>
+          <button
+            onClick={() => {
+              setLengthFilter(['length', 'Short']);
+              filterQuestion();
+            }}
+          >
+            Short
+          </button>
+          <button
+            onClick={() => {
+              setLengthFilter(['length', 'Long']);
+              filterQuestion();
+            }}
+          >
+            Long
+          </button>
         </div>
-
-        <h3>Test button</h3>
-
-        <button onClick={() => filterQuestion('calculator', 'No Calculator')}>
-          Submit Calculator
-        </button>
-        <button onClick={() => filterQuestion('answer_type', 'Grid-In')}>
-          Submit Answer Type
-        </button>
-        <button onClick={() => filterQuestion('chart', 'No Chart')}>
-          Submit Chart Type
-        </button>
-        <button onClick={() => filterQuestion('length', 'Long')}>
-          Submit Question Length
-        </button>
       </div>
 
       {questions.slice(0, 10).map((e) => {
